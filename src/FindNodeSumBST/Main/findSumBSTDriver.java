a65a2ee1e9d0ded4407e24f7554939b6e922dddb
ChitraSingh2023GL <146869240+ChitraSingh2023GL@users.noreply.github.com>
2023-11-07 22:15:03 +0530

    Add files via upload

---
+++ src/FindNodeSumBST/Main/findSumBSTDriver.java
@@ -0,0 +1,44 @@
+package FindNodeSumBST.Main;
+import java.util.Scanner;
+
+import FindNodeSumBST.Service.Node;
+import FindNodeSumBST.Service.insertNodeBST;
+
+public class findSumBSTDriver {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			Node root = null;
+			insertNodeBST FP = new insertNodeBST();
+			boolean result;
+			int key;
+			root = FP.insertNode(root, 40);
+			root = FP.insertNode(root, 20);
+			root = FP.insertNode(root, 60);
+			root = FP.insertNode(root, 10);
+			root = FP.insertNode(root, 30);
+			root = FP.insertNode(root, 50);
+			root = FP.insertNode(root, 70);
+			FP.inOrderTraverse(root);
+
+			System.out.println("\nEnter value which pair find in  given above  tree.");
+			if (sc.hasNextInt()) {
+				key = sc.nextInt();
+				result = FP.findSumPair(key);
+				if (!result) {
+					System.out.println("nodes are not found.");
+				}
+
+			} else {
+				System.out.println("enter value in integer format.");
+			}
+
+		} catch (Exception ex) {
+			System.out.println("Please check details of runtime error.\n" + ex.getMessage());
+		} finally {
+			sc.close();
+		}
+
+	}
+
+}
